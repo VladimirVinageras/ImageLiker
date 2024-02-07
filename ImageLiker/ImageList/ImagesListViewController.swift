46cd1bbad24ea3cf635ae0c0cc923a667d0db21f
aVVin <vvinageras@gmail.com>
2024-02-08 01:13:24 +0200

    Update ImagesListViewController.swift

--- ImageLiker/ImageList/ImagesListViewController.swift
+++ ImageLiker/ImageList/ImagesListViewController.swift
@@ -29,7 +29,7 @@ class ImagesListViewController: UIViewController {
         tableView.dataSource = self
         tableView.delegate = self
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
-    //    tableView.register(ImagesListCell.self, forCellReuseIdentifier: ImagesListCell.reuseIdentifier)
+
     }
 
     
