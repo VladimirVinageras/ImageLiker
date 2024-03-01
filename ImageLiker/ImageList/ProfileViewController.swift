32fd9d2b1883932adf9a0943e345f1fb43e85ade
Vladimir Vinakheras <vvinageras@gmail.com>
2024-03-02 00:45:31 +0200

    Ready

--- ImageLiker/ImageList/ProfileViewController.swift
+++ ImageLiker/ImageList/ProfileViewController.swift
@@ -9,11 +9,106 @@ import Foundation
 import UIKit
 
 class ProfileViewController: UIViewController {
+    var userNameLabel = UILabel()
+    var userLoginLabel = UILabel()
+    var messageLabel = UILabel()
+    var profileImage : UIImage?
+    var imageView : UIImageView?
+    var logOutButton : UIButton?
     
-    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        createUserProfileImageView(systemNameImage: "person.crop.circle.fill")
+        createLogoutButton(buttonImageSystemName: "ipad.and.arrow.forward")
+        createUserNameLabel(userName: "Ekaterina Vinakheras")
+        createUserLoginLabel(userLogin: "@ekaterina_vin")
+        createMessageLabel(message: "Hello, IOS15!")
+        activateConstraints()
+        
     }
+    
+    private func createUserProfileImageView(systemNameImage : String){
+        profileImage = UIImage(named: "profile")
+        imageView = UIImageView(image: profileImage)
+        guard let imageView = imageView else {return}
+     //   imageView.tintColor = .gray
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+    }
+    
+    private func createUserNameLabel(userName: String){
+        userNameLabel.text = userName
+        userNameLabel.textColor = .ypWhite
+            userNameLabel.font = UIFont.boldSystemFont(ofSize: 23)
+    
+       
+        userNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userNameLabel)
+    }
+    private func createUserLoginLabel(userLogin: String){
+        userLoginLabel.text = userLogin
+     
+            userLoginLabel.font = UIFont.systemFont(ofSize: 13)
+               
+        userLoginLabel.textColor = .ypGray
+        userLoginLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(userLoginLabel)
+    }
+    
+    private func createMessageLabel(message: String){
+        
+        messageLabel.text = message
+        messageLabel.textColor = .ypWhite
+        messageLabel.font = UIFont.systemFont(ofSize: 13)
+        messageLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(messageLabel)
+    }
+    
+    private func createLogoutButton(buttonImageSystemName : String){
+        logOutButton = UIButton.systemButton(
+            with: UIImage(systemName: buttonImageSystemName)!,
+            target: self,
+            action: #selector(Self.didTapButton)
+        )
+        guard let logOutButton = logOutButton else {return}
+        logOutButton.tintColor = .red
+        logOutButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logOutButton)
+    }
+    
+    private func activateConstraints(){
+        guard let imageView = imageView else {return}
+        guard let button = logOutButton else {return}
+        NSLayoutConstraint.activate([
+            imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 30),
+            imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            imageView.widthAnchor.constraint(equalToConstant: 70),
+            imageView.heightAnchor.constraint(equalToConstant: 70),
+            userNameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8),
+            userNameLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            userLoginLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 8),
+            userLoginLabel.leadingAnchor.constraint(equalTo: userNameLabel.leadingAnchor),
+            messageLabel.topAnchor.constraint(equalTo: userLoginLabel.bottomAnchor, constant: 8),
+            messageLabel.leadingAnchor.constraint(equalTo: userNameLabel.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -24),
+            button.centerYAnchor.constraint(equalTo: imageView.centerYAnchor)
+        ])
+    }
+    
+    
+    @objc
+    private func didTapButton(){
+      print("I'll miss you 🥺")
+    }
+    
+    
+    
+    
+    
 }
 
 
--- ImageLiker/ImageList/SingleImageViewController.swift
+++ ImageLiker/ImageList/SingleImageViewController.swift
@@ -11,15 +11,92 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     var image : UIImage?{
         didSet {
-            guard isViewLoaded else { return } // 1
-            imageView.image = image // 2
+            guard isViewLoaded else { return }
+            imageView.image = image
+            guard let image = image else { return }
+                self.rescaleAndCenterImageInScrollView(image: image)
+            
         }
     }
-    @IBOutlet private weak var imageView: UIImageView!
+    //MARK: - Outlets
     
+    @IBOutlet private weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+   
+    //MARK: - "Must be" functions
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         imageView.image = image
+        imageView.contentMode = .scaleAspectFit
+        
+        scrollView.minimumZoomScale = 0.5
+        scrollView.maximumZoomScale = 1.5
+        guard let image = image else { return }
+        self.rescaleAndCenterImageInScrollView(image: image)
+    }
+    //MARK: - Actions
+    @IBAction func didTapBackButton(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+   
+    
+    @IBAction func didTapShareButton(_ sender: Any) {
+       shareImage()
+    }
+    
+}
+
+
+//MARK: - EXTENSIONS
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        let xOffset = max((scrollView.bounds.size.width - scrollView.contentSize.width) * 0.5, 0)
+        let yOffset = max((scrollView.bounds.size.height - scrollView.contentSize.height) * 0.5, 0)
+        scrollView.contentInset = UIEdgeInsets(top: yOffset, left: xOffset, bottom: 0, right: 0)
+    }
+    
+    func scrollViewDidEndZooming(_ scrollView: UIScrollView, with view: UIView?, atScale scale: CGFloat) {
+        scrollViewDidZoom(scrollView)
+    }
+}
+
+extension SingleImageViewController {
+    
+    private func rescaleAndCenterImageInScrollView(image: UIImage) {
+        let minZoomScale = scrollView.minimumZoomScale
+        let maxZoomScale = scrollView.maximumZoomScale
+        view.layoutIfNeeded()
+        let visibleRectSize = scrollView.bounds.size
+        let imageSize = image.size
+        let hScale = visibleRectSize.width / imageSize.width
+        let vScale = visibleRectSize.height / imageSize.height
+        let scale = min(maxZoomScale, max(minZoomScale, min(hScale, vScale)))
+        scrollView.setZoomScale(scale, animated: false)
+        scrollView.layoutIfNeeded()
+        let newContentSize = scrollView.contentSize
+        let x = (newContentSize.width - visibleRectSize.width) / 2
+        let y = (newContentSize.height - visibleRectSize.height) / 2
+        scrollView.setContentOffset(CGPoint(x: x, y: y), animated: false)
+    }
+}
+
+extension SingleImageViewController {
+    private func shareImage(){
+        guard let image = imageView.image else {
+                   return
+               }
+               let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+               activityViewController.excludedActivityTypes = [UIActivity.ActivityType.addToReadingList]
+               present(activityViewController, animated: true, completion: nil)
     }
 }
